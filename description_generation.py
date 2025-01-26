3e8cb142bf3a87e024cb69a2f05e44e3cf719c6c
AysanAghazadeh <aya34@pitt.edu>
2025-01-26 08:49:02 -0500

    changed

--- description_generation.py
+++ description_generation.py
@@ -54,7 +54,7 @@ def get_single_description(args, image_url, pipe):
     print(outputs)
     if args.VLM == 'InternVL' or args.VLM == 'LLAVA16' or args.VLM == 'RLHFV':
         description = outputs
-    elif args.VLM == 'QWenVl':
+    elif args.VLM == 'QWenVL':
         description = outputs[0]
     else:
         description = outputs[0]['generated_text'].split('ASSISTANT: ')[-1]
