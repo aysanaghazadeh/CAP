a4920ec80fef033c00f46f1e2b3c0efa002ae489
AysanAghazadeh <aya34@pitt.edu>
2025-01-27 23:56:54 -0500

    changed

--- Mistral7B_training.py
+++ Mistral7B_training.py
@@ -16,6 +16,7 @@ def get_model():
     )
     model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1",
                                                  device_map='auto',
+                                                 token='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',
                                                  quantization_config=bnb_config)
     model.gradient_checkpointing_enable()
     model = prepare_model_for_kbit_training(model)
@@ -29,7 +30,8 @@ def get_model():
     if torch.cuda.device_count() > 1:
         model.is_parallelizable = True
         model.model_parallel = True
-    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1",
+                                              token='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')
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "right"
     return model, tokenizer
