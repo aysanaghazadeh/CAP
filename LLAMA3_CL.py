b44b84b708794e0ec4dab1479132a7fee93d1e6b
AysanAghazadeh <aya34@pitt.edu>
2025-01-27 23:41:15 -0500

    changed

--- LLAMA3_CL.py
+++ LLAMA3_CL.py
@@ -6,6 +6,7 @@ import torch
 from peft import get_peft_model, LoraConfig, TaskType, prepare_model_for_kbit_training
 from trl import CPOConfig, CPOTrainer, ModelConfig, get_peft_config
 import os
+import transformers
 
 def get_model():
     bnb_config = BitsAndBytesConfig(
@@ -14,9 +15,11 @@ def get_model():
         # bnb_4bit_use_double_quant=True,
         bnb_8bit_compute_dtype=torch.bfloat16
     )
-    model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-instruct",
+    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-7B-Instruct",
+                                                 # "meta-llama/Meta-Llama-3-8B-instruct",
                                                  token='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',
                                                  device_map='auto',
+                                                 trust_remote_code=True,
                                                  quantization_config=bnb_config)
     model.gradient_checkpointing_enable()
     model = prepare_model_for_kbit_training(model)
@@ -31,7 +34,9 @@ def get_model():
         print(f'torch cuda count: {torch.cuda.device_count()}')
         model.is_parallelizable = True
         model.model_parallel = True
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-instruct",
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-7B-Instruct",
+                                              # "meta-llama/Meta-Llama-3-8B-instruct",
+                                              trust_remote_code=True,
                                               token='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')
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "right"
@@ -40,7 +45,7 @@ def get_model():
 
 def get_training_args(args):
     training_args = CPOConfig(
-        output_dir=args.model_path+'/my_LLAMA3_CPO',
+        output_dir=args.model_path+'/my_QWenLM_CPO',
         remove_unused_columns=False,
         per_device_train_batch_size=args.batch_size,
         gradient_checkpointing=True,
@@ -53,6 +58,7 @@ def get_training_args(args):
         save_steps=50,
         evaluation_strategy="steps",
         eval_steps=10,
+        max_length=20,
         do_eval=True,
         label_names=["input_ids", "labels", "attention_mask"],
         report_to="none",
@@ -68,7 +74,7 @@ if __name__ == '__main__':
     cpo_args = get_training_args(args)
     model, tokenizer = get_model()
     cpo_config = CPOConfig(beta=0.1,
-                           output_dir=args.model_path+'/my_LLAMA3_CPO',)
+                           output_dir=args.model_path+'/my_QWenLM_CPO',)
     train_dataset = get_train_LLAMA3_CPO_Dataloader(args)
     tmp = train_dataset.train_test_split(test_size=0.1)
     train_dataset = tmp["train"]
