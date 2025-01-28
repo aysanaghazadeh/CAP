4bbe9a9d9920fbbe62d1541b080a0c7af4a8fb7a
AysanAghazadeh <aya34@pitt.edu>
2025-01-27 23:14:24 -0500

    changed

--- util/data/data_util.py
+++ util/data/data_util.py
@@ -171,8 +171,9 @@ def get_train_LLAMA3_instruct_Dataloader(args):
 
 
 def get_LLAMA3_CPO_training_data(args, image_urls):
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-instruct",
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-7B-Instruct",#"meta-llama/Meta-Llama-3-8B-instruct",
                                               token='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',
+                                              trust_remote_code=True,
                                               padding='right')
     tokenizer.pad_token = tokenizer.eos_token
     if tokenizer.chat_template is None:
@@ -189,8 +190,8 @@ def get_LLAMA3_CPO_training_data(args, image_urls):
     negative_QAs = {}
     negative_QAs['reason'] = json.load(
         open(os.path.join(args.data_path, 'train/reason_hard_QA_Combined_Action_Reason_train.json')))
-    # negative_QAs['action']= json.load(
-    #     open(os.path.join(args.data_path, 'train/action_hard_QA_Combined_Action_Reason_train.json')))
+    negative_QAs['action']= json.load(
+        open(os.path.join(args.data_path, 'train/action_hard_QA_Combined_Action_Reason_train.json')))
     negative_QAs['adjective'] = json.load(
         open(os.path.join(args.data_path, 'train/adjective_hard_QA_Combined_Action_Reason_train.json')))
     negative_QAs['semantic'] = json.load(
@@ -226,11 +227,9 @@ def get_LLAMA3_CPO_training_data(args, image_urls):
 
 def get_train_LLAMA3_CPO_Dataloader(args):
     image_urls = list(json.load(open(os.path.join(args.data_path,
-                                     'train/reason_hard_QA_Combined_Action_Reason_train.json'))).keys())
+                                                  'train/reason_hard_QA_Combined_Action_Reason_train.json'))).keys())
     dataset = get_LLAMA3_CPO_training_data(args, image_urls)
     return dataset
-
-
 
 def get_LLAMA3_RLHF_training_data(args, image_urls):
     tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-V2-Chat",#"meta-llama/Meta-Llama-3-8B",
