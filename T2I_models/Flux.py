be3d0c8eef6eec6ea8973798e4b5b44b6461f4ed
AysanAghazadeh <aya34@pitt.edu>
2025-02-18 02:54:53 -0500

    changed

--- T2I_models/Flux.py
+++ T2I_models/Flux.py
@@ -13,15 +13,9 @@ class Flux(nn.Module):
             bnb_8bit_compute_dtype=torch.float16
         )
         self.pipeline = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev",
-                                                     torch_dtype=torch.float16,
-                                                     quantization_config=quantization_config)
+                                                     device_map='auto')
 
     def forward(self, prompt):
         image = self.pipeline(prompt,
-                              height=512,
-                              width=512,
-                              guidance_scale=3.5,
-                              num_inference_steps=28,
-                              max_sequence_length=512
                               ).images[0]
         return image
