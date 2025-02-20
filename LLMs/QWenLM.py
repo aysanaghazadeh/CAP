cfa4e63f25b4794a239ecb364d2be8641f9c6fca
AysanAghazadeh <aya34@pitt.edu>
2025-02-19 21:12:03 -0500

    changed

--- LLMs/QWenLM.py
+++ LLMs/QWenLM.py
@@ -18,7 +18,7 @@ class QWenLM(nn.Module):
             self.tokenizer.padding_side = "right"
             self.model = PeftModel.from_pretrained(self.model,
                                                    os.path.join(args.model_path,
-                                                                'my_QWenLM_CPO/checkpoint-1950/'))
+                                                                'my_QWenLM_CPO/checkpoint-2200/'))
         else:
             self.model = AutoModelForCausalLM.from_pretrained(
                 model_name,
