37d903b14ae39d004e4ceb8f9accc7a3cc6ef55b
AysanAghazadeh <aya34@pitt.edu>
2025-02-19 21:18:18 -0500

    changed

--- test.py
+++ test.py
@@ -102,7 +102,7 @@ format = '''The interpretation format is: I ${should or shouldn't} ${action} bec
 prompt = f"""What is the correct interpretation for the described image:
 
              Description: {description}.
-             {format}"""
+             """
 
 pipe = LLM(args)
 print(pipe(prompt))
