453db21573a24e4d3b20b6dd65f7d2bdd27ea815
AysanAghazadeh <aya34@pitt.edu>
2025-03-04 03:29:43 -0700

    changed

--- description_generation.py
+++ description_generation.py
@@ -138,7 +138,7 @@ def get_llm_generated_prompt(args, test_images):
     print(f'number of images in test set: {len(test_images)}')
     print('*' * 100)
     description_file = os.path.join(args.data_path,
-                                    f'train/{args.llm_prompt.replace(".jinja", f"_{args.LLM}_FT{args.fine_tuned}")}_PSA.csv')
+                                    f'train/{args.llm_prompt.replace(".jinja", f"_{args.LLM}_FT{args.fine_tuned}")}_{args.AD_type}.csv')
     if os.path.exists(description_file):
         processed_images = set(pd.read_csv(description_file).ID.values)
         # return pd.read_csv(description_file)
