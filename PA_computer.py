ce9beedc3c2e986c1824e4d23efe508720b2ccb8
AysanAghazadeh <aya34@pitt.edu>
2025-03-06 17:14:29 -0500

    changed

---
+++ PA_computer.py
@@ -0,0 +1,27 @@
+import json
+
+while True:
+    AIM_path = input('AIM path:')
+    PA_path = input('PA path')
+    AIM = json.load(open(AIM_path))
+    pa = json.load(open(PA_path))
+    sum_val, image_count = 0, 0
+    for image_url in AIM:
+        # AIM = sum(LLAMA_PSA_AIM[image_url][-1])/len(LLAMA_PSA_AIM[image_url][-1]) if LLAMA_PSA_AIM[image_url][-1] != 0 else 0
+        AIM = AIM[image_url][-1][0] if AIM[image_url][-1] != 0 else 0
+        if AIM > 1:
+            print(AIM, image_url)
+        PA = pa[image_url]
+        score, count = 0, 0
+        for i, value in enumerate(PA):
+            if i in [1, -3]:
+                continue
+            score += value / 5
+            count += 1
+        score += AIM * 2
+        count += 2
+        # print(score/count, count)
+        sum_val += score / count
+        image_count += 1
+    print(sum_val / image_count, image_count)
+    print('-' * 150)
