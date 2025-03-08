8529233b914893e533f25ce40d8942a1ad062b73
AysanAghazadeh <aya34@pitt.edu>
2025-03-07 22:02:27 -0500

    changed

--- evaluate.py
+++ evaluate.py
@@ -593,8 +593,65 @@ class Evaluation:
         image_text_alignment_scores = json.load(open(os.path.join(args.result_path,
                                                                   args.text_alignment_file)))
         action_reason_file = json.load(open(os.path.join(args.data_path, args.test_set_QA)))
+
+        first_image_list = [
+            "6/143816.jpg",
+            "8/7608.jpg",
+            "4/102794.jpg",
+            "4/134044.jpg",
+            "6/17896.jpg",
+            "7/116347.jpg",
+            "7/46267.jpg",
+            "6/90766.jpg",
+            "6/17276.jpg",
+            "3/134973.jpg",
+            "2/116492.jpg",
+            "0/73110.jpg",
+            "0/45970.jpg",
+            "0/41530.jpg"
+        ]
+
+        second_image_list = [
+            "0/41530.jpg",
+            "5/71965.jpg",
+            "0/116140.jpg",
+            "1/57011.jpg",
+            "3/134033.jpg",
+            "5/71815.jpg",
+            "1/112811.jpg",
+            "3/81633.jpg",
+            "4/134044.jpg",
+            "6/143816.jpg",
+            "7/41457.jpg"
+        ]
+
+        third_image_list = [
+            "8/138668.jpg", "8/138668.jpg", "8/138668.jpg",
+            "8/121068.jpg", "8/121068.jpg", "8/121068.jpg",
+            "8/57788.jpg", "8/57788.jpg", "8/57788.jpg",
+            "7/33807.jpg", "7/33807.jpg", "7/33807.jpg",
+            "4/129394.jpg", "4/129394.jpg", "4/129394.jpg",
+            "4/78954.jpg", "4/78954.jpg", "4/78954.jpg",
+            "4/46914.jpg", "4/46914.jpg", "4/46914.jpg",
+            "2/146302.jpg", "2/146302.jpg", "2/146302.jpg",
+            "2/124992.jpg", "2/124992.jpg", "2/124992.jpg",
+            "2/57982.jpg", "2/57982.jpg", "2/57982.jpg",
+            "0/111880.jpg", "0/111880.jpg", "0/111880.jpg",
+            "0/24680.jpg", "0/24680.jpg", "0/24680.jpg",
+            "8/52488.jpg", "8/52488.jpg", "8/52488.jpg",
+            "4/84094.jpg", "4/84094.jpg", "4/84094.jpg",
+            "4/46914.jpg", "4/46914.jpg", "4/46914.jpg",
+            "6/14807"]
+        image_set = input('which image set:')
+        if image_set == 'first':
+            image_list = first_image_list
+        else:
+            image_list = third_image_list
+
         for row in range(len(results.values)):
             image_url = results.image_url.values[row]
+            if image_url not in image_list:
+                continue
             if image_url not in image_text_alignment_scores:
                 continue
             action_reasons = action_reason_file[image_url][0]
@@ -617,16 +674,16 @@ class Evaluation:
                 f'creativity score for image {image_url} is {creativity_scores[image_url]}')
             # if image_url in ['5/102155.jpg', '9/76489.jpg', '5/6125.jpg', '1/29981.jpg']:
             #     print_list.append(f'objects for {image_url} are {get_objects(action_reasons)}')
-            print(f'number of images with no product image is: {no_product_image_count}')
-            print(f'average creativity score: {sum(creativity_scores.values()) / len(list(creativity_scores.keys()))}')
-            print('\n - '.join(print_list))
-            creativity_scores['average'] = sum(creativity_scores.values()) / len(list(creativity_scores.keys()))
-            with open(saving_path, "w") as outfile:
-                json.dump(creativity_scores, outfile)
-
         print(f'number of images with no product image is: {no_product_image_count}')
-        print(f'average creativity score: {creativity_scores["average"]}')
+        print(f'average creativity score: {sum(creativity_scores.values()) / len(list(creativity_scores.keys()))}')
         print('\n - '.join(print_list))
+        creativity_scores['average'] = sum(creativity_scores.values()) / len(list(creativity_scores.keys()))
+        # with open(saving_path, "w") as outfile:
+        #     json.dump(creativity_scores, outfile)
+
+        # print(f'number of images with no product image is: {no_product_image_count}')
+        # print(f'average creativity score: {creativity_scores["average"]}')
+        # print('\n - '.join(print_list))
 
     def evaluate_action_reason_VLM(self, args):
         results = {'acc@1': 0, 'acc@2': 0, 'acc@3': 0,
